6473ec54f2b8a8581458ca96b270c34a32ee4f23
AlCalzone <d.griesel@gmx.net>
2022-08-18 16:10:37 +0200

    feat: convert to hybrid package (#1)

---
+++ .github/test-scripts/test.cjs
@@ -0,0 +1,4 @@
+const { execa } = require("@esm2cjs/execa");
+const assert = require("assert");
+
+assert(typeof execa === "function");
---
+++ .github/test-scripts/test.mjs
@@ -0,0 +1,4 @@
+import { execa } from "@esm2cjs/execa";
+import assert from "assert";
+
+assert(typeof execa === "function");
